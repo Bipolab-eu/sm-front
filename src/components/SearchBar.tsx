b9d63e7cbcd41aae622db49493ddac096130e5ca
bipolab-eu <bentagai@bipolab.eu>
2024-08-07 17:03:40 +0100

    mejoras de rendimiento en buscador

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -6,7 +6,7 @@
 */
 
 import { findOneCollege } from "@/lib/api"
-import { useState } from "react"
+import { useState, Suspense } from "react"
 import { useDebounce } from '../hooks/useDebounce';
 
 interface Props {
@@ -33,7 +33,7 @@ export default function SearchBar({ register, setValue, watch, errors }:Props) {
         }
       }
     },
-    500,
+    250,
     [watchCollege]
   );
 
@@ -59,30 +59,37 @@ export default function SearchBar({ register, setValue, watch, errors }:Props) {
           <span className="text-red-500 text-xs leading-none align-top">*</span>
           Busca el nombre de tu instituto
         </label>
-        <input
-          id="college"
-          name="college"
-          type="search"
-          className="block w-full px-4 py-2 rounded-lg border-solid border border-slate-950 bg-slate-100 transition duration-200 ease-in-out"
-          placeholder="Ejemplo: IES LA MINILLA"
-          onFocus={handleFocus}
-          onBlur={handleBlur}
-          {...register('college')}
-        />
-        {errors.college && <span className="text-red-500 text-xs">{errors.college.message}</span>}
 
-        <div className={`${ hideSuggestions } 'hidden'`}>
-          {suggestions.map((e: any) => (
-            <div
-              className="px-4 py-2 rounded-lg bg-slate-100"
-              key={e.id}
-              onClick={() => handleSuggestionClick(e.name, e.id)}
-            >
-              {e.name} hola
-            </div>
-          ))}
+        <div onBlur={handleBlur}>
+          <input
+            id="college"
+            name="college"
+            className="block w-full px-4 py-2 rounded-lg border-solid border border-slate-950 bg-slate-100 transition duration-200 ease-in-out"
+            placeholder="Ejemplo: IES LA MINILLA"
+            onFocus={handleFocus}
+            // onBlur={handleBlur}
+            {...register('college')}
+          />
+          {errors.college && <span className="text-red-500 text-xs">{errors.college.message}</span>}
         </div>
+        <Suspense fallback={<Loading />}>
+          <div className={`${ hideSuggestions === true ? 'hidden' : 'block' } pt-2`}>
+            {suggestions.map((e: any) => (
+              <div
+                className="px-4 py-2 rounded-lg bg-slate-100 mb-2 active:bg-slate-950 active:text-slate-100"
+                key={e.id}
+                onClick={() => handleSuggestionClick(e.name, e.id)}
+              >
+                {e.name}
+              </div>
+            ))}
+          </div>
+        </Suspense>
       </fieldset>
     )
+}
+
+function Loading() {
+  return <h2>🌀 Loading...</h2>;
 }
 /* Ver tutorial: https://upmostly.com/tutorials/how-to-create-a-search-bar-in-react */
